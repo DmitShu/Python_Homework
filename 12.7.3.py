16ab8ce370b7812088100aa22a5e5353d1a6e6fa
DmitShu <dmitshu@gmail.com>
2022-03-13 18:29:24 +0300

    Hidden strings

--- 12.7.3.py
+++ 12.7.3.py
@@ -1,16 +1,16 @@
-#Ввод чисел и сортировка
-num = int(input('Введите число:'))
-data=[]
-while num !=0:
-    data.append(num)
-    num = int(input('Введите число:'))
-data.sort()
-print(data)
-wrd = input('Введите:')
-data=[]
-while wrd != '':
-    if wrd not in data:
-        data.append(wrd)
-    wrd = input('Введите:')
-for it in data:
-    print(it)
+# #Ввод чисел и сортировка
+# num = int(input('Введите число:'))
+# data=[]
+# while num !=0:
+#     data.append(num)
+#     num = int(input('Введите число:'))
+# data.sort()
+# print(data)
+# wrd = input('Введите:')
+# data=[]
+# while wrd != '':
+#     if wrd not in data:
+#         data.append(wrd)
+#     wrd = input('Введите:')
+# for it in data:
+#     print(it)
