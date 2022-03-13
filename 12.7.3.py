b7eb60103cb92796747da92cd7fb194dfb5788c3
DmitShu <dmitshu@gmail.com>
2022-03-13 19:18:57 +0300

    Версия 1

--- 12.7.3.py
+++ 12.7.3.py
@@ -14,3 +14,24 @@
 #     wrd = input('Введите:')
 # for it in data:
 #     print(it)
+
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+print(69*'*')
+print("Уважаемый клиент, вам доступны следующие условия:")
+print()
+for bnk, per in per_cent.items():
+    print(f'Банк "{bnk}"\t Процент по вкладу: {per}')
+print(69*'*')
+mny = float(input('Введите сумму, которую вы планируете внести: '))
+print('Что вы получите через год:')
+for bnk, per in per_cent.items():
+     print(f'Банк "{bnk}"\t Сумма на счету: {round((per/100+1)*mny, 2)}\t Ваш доход: {round(per*mny/100, 2)}')
+print(69*'*')
+print('Лучшие условия:')
+print(69*'*')
+# r = 0
+# for k in per_cent:
+#     if per_cent[k] > r:
+#         r = per_cent[k]
+#         m = k
+# print('макс:', m, round(r * money / 100, 2))
