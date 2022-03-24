ae3a9f062b548e6a37a5ab06a86819a61e5aadc9
DmitShu <dmitshu@gmail.com>
2022-03-24 19:44:51 +0300

    14 start

--- 14.py
+++ 14.py
@@ -54,4 +54,15 @@ def char_frequency():
 ...
 
 # вызвали функцию в любом месте программы
-char_frequency()
+#char_frequency()
+
+def add2and2():
+    print (2+2)
+#add2and2()
+
+# функция, которая возводит любое число в степень n
+def pow_func(base, n=2):
+   print(base ** n)
+
+pow_func(3)  # 9
+pow_func(5, 3)  # 125
