5b9f21d269fe309377df3ebf670f3454eab37760
DmitShu <dmitshu@gmail.com>
2022-03-27 16:10:13 +0300

    14.6

--- 14.5.py
+++ 14.5.py
@@ -18,3 +18,36 @@ for a in e(): # e() - генератор
         last = a # иначе - присваиваем новое значение
 L = ['THIS', 'IS', 'LOWER', 'STRING']
 print(list(map(str.lower, L)))
+
+# n=0
+#
+# n = int(input())
+#
+# print(n % 2)
+def even(x):
+   return x % 2 == 0
+
+result = filter(even, [-2, -1, 0, 1, -3, 2, -3])
+
+print(list(result))   # [-2, 0, 2]
+
+# map + filter
+some_list = [i - 10 for i in range(20)]
+def pow2(x): return x**2
+def positive(x): return x > 0
+
+print(some_list)
+print(list(map(pow2, filter(positive, some_list))))
+
+# (вес, рост)
+data = [
+   (82, 191),
+   (68, 174),
+   (90, 189),
+   (73, 179),
+   (76, 184)
+]
+
+# Из списка в предыдущем задании найдите кортеж с минимальным индексом массы тела
+
+print(min(data, key=lambda x: x[0] / x[1] ** 2))  # отбор по ключу
