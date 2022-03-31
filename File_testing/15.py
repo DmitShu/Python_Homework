818dc3ceaa564783bd4a60a8de6253253ee79f61
DmitShu <dmitshu@gmail.com>
2022-03-31 18:31:40 +0300

    15 1 3

--- File_testing/15.py
+++ File_testing/15.py
@@ -5,3 +5,7 @@ print(myFile.readlines())
 myFile = open('File1.txt', 'rt', encoding='utf8')
 for line in myFile:
     print(line)
+
+myFile2 = open('File2.txt', 'w', encoding='utf8')
+myFile2.write('ttt')
+print('zzz', file=myFile2)
