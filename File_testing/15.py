fc05d7224281d423c0b5a481cfc83555b7dab7a2
DmitShu <dmitshu@gmail.com>
2022-03-31 18:25:18 +0300

    15 1 3

--- File_testing/15.py
+++ File_testing/15.py
@@ -1,2 +1,7 @@
 myFile = open('File1.txt', 'rt', encoding='utf8')
-print(myFile.read())
+print(myFile.readline())
+myFile = open('File1.txt', 'rt', encoding='utf8')
+print(myFile.readlines())
+myFile = open('File1.txt', 'rt', encoding='utf8')
+for line in myFile:
+    print(line)
