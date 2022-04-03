8fa50aa5539bb49a324c9bed351e6a0c61cf1e7e
DmitShu <dmitshu@gmail.com>
2022-04-03 19:04:09 +0300

    some big test thing json

--- File_testing/jsontests.py
+++ File_testing/jsontests.py
@@ -9,11 +9,12 @@ import json
 #
 # with open('tmpw.json', encoding='utf8') as f:
 #     print(f.read())
-items = {'timestamp': 'int', 'referer': 'url', 'location': 'url', 'remoteHost': 'str', 'partyId': 'str',
+itemslist = {'timestamp': 'int', 'referer': 'url', 'location': 'url', 'remoteHost': 'str', 'partyId': 'str',
           'sessionId': 'str', 'pageViewId': 'str', 'eventType': 'val', 'item_id': 'str', 'item_price': 'int',
           'item_url': 'url', 'basket_price': 'str', 'detectedDuplicate': 'bool', 'detectedCorruption': 'bool',
           'firstInSession': 'bool', 'userAgentName': 'str'}
 
+Error = []
 
 with open('real.json', encoding='utf8') as f:
      tmpl = json.load(f)
@@ -30,9 +31,46 @@ def CheckStr(item):
 def CheckBool(item):
      return isinstance(item, bool)
 
-
 def CheckUrl(item):
      if isinstance(item, str):
           return item.startswith('http:\\') or item.startswith('https:\\')
      else:
           return False
+
+def CheckStrValue(item, val):
+    if isinstance(item, str):
+        return item in val
+    else:
+        return False
+
+def ErrorLog(item, value, string):
+    Error.append({item: f'{value}, {string}'})
+
+for items in tmpl:
+    for item in items:
+        if item in itemslist:
+            if itemslist[item] == 'int':
+                if not CheckInt(items(item)):
+                    ErrorLog(item, items[item], f' ожидали тип {itemslist[item]}')
+            elif itemslist[item] == 'str':
+                if not CheckStr(items(item)):
+                    ErrorLog(item, items[item], f' ожидали тип {itemslist[item]}')
+            elif itemslist[item] == 'bool':
+                if not CheckBool(items(item)):
+                    ErrorLog(item, items[item], f' ожидали тип {itemslist[item]}')
+            elif itemslist[item] == 'url':
+                if not CheckUrl(items(item)):
+                    ErrorLog(item, items[item], f' ожидали тип {itemslist[item]}')
+            elif itemslist[item] == 'val':
+                if not CheckStrValue(items[item], ['itemBuyEvent', 'itemViewEvent']):
+                    ErrorLog(item, items[item], ' ожидали itemBuyEvent или itemViewEvent')
+            else:
+                ErrorLog(item, items[item], 'неожиданное значение')
+        else:
+            ErrorLog(item, items[item], 'неизвестная переменная')
+
+if Error == []:
+    print('Pass')
+else:
+    print('Fail')
+    print(Error)
