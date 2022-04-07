916823f2bc3da9b6dbd6979727f33c79b8ef7f66
DmitShu <dmitshu@gmail.com>
2022-04-07 19:31:24 +0300

    OOP testing

--- OOP/16.3.py
+++ OOP/16.3.py
@@ -1,12 +1,25 @@
+# class User:
+#     number_of_fingers = 5
+#     number_of_eyes = 2
+#
+# peter = User()
+# peter.name = "Peter Robertson"
+#
+# julia = User()
+# julia.name = "Julia Donaldson"
+#
+# print(peter.name)
+# print('\n')
+# print(julia.name)
+# print(julia.number_of_fingers)
+
 class User:
-    pass
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
-peter = User()
-peter.name = "Peter Robertson"
-
-julia = User()
-julia.name = "Julia Donaldson"
+peter = User(name="Peter Robertson", email="peterrobertson@mail.com")
+julia = User(name="Julia Donaldson", email="juliadonaldson@mail.com")
 
 print(peter.name)
-print('\n')
-print(julia.name)
+print(julia.email)
