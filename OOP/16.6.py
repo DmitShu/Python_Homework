1f7e44e9ca64f85360faf983a2c129c74c57a726
DmitShu <dmitshu@gmail.com>
2022-04-08 08:40:05 +0300

    16,6

--- OOP/16.6.py
+++ OOP/16.6.py
@@ -1,5 +1,4 @@
 import datetime
-
 class Product:
     max_quantity = 100000
 
@@ -21,3 +20,36 @@ eggs = Food(name="eggs", category="food", quantity_in_stock=5)
 print(eggs.max_quantity)
 print(eggs.is_available())
 print(eggs.refresh_frequency)
+
+class Event:
+    def __init__(self, timestamp=0, event_type="", session_id=""):
+        self.timestamp = timestamp
+        self.type = event_type
+        self.session_id = session_id
+
+    def init_from_dict(self, event_dict):
+        self.timestamp = event_dict.get("timestamp")
+        self.type = event_dict.get("type")
+        self.session_id = event_dict.get("session_id")
+
+    def show_description(self):
+        print("This is generic event.")
+
+
+class ItemViewEvent(Event):
+    type = "itemViewEvent"
+
+    def __init__(self, timestamp=0, session_id="", number_of_views=0):
+        self.timestamp = timestamp
+        self.session_id = session_id
+        self.number_of_views = number_of_views
+
+    def show_description(self):
+        print("This event means someone has browsed an item.")
+
+
+if __name__ == "__main__":
+    test_view_event = ItemViewEvent(timestamp=1549461608000, session_id="0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct", number_of_views=6)
+    test_view_event.show_description()
+    print(test_view_event.type)
+
