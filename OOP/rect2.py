5bdf83c4d27e48a421bc5a278c531976c848bad6
DmitShu <dmitshu@gmail.com>
2022-04-09 15:26:37 +0300

    16,8

---
+++ OOP/rect2.py
@@ -0,0 +1,13 @@
+from rect import Rectangle, Square
+
+rect_1 = Rectangle(3,4)
+rect_2 = Rectangle(12,5)
+
+print(rect_1.get_area())
+print(rect_2.get_area())
+
+square_1 = Square(5)
+square_2 = Square(10)
+
+print(square_1.get_area())
+print(square_2.get_area())
