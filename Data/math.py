d5981babb24f88891120836c54821090c7745030
DmitShu <dmitshu@gmail.com>
2022-04-14 20:09:48 +0300

    17 test

--- Data/math.py
+++ Data/math.py
@@ -31,4 +31,19 @@ def par_checker(string):
     # значит, возвращаем True, иначе — False
     return len(stack) == 0
 
-print(par_checker('((('))
+
+pars = {")": "(", "]": "["}
+
+
+def par_checker_mod(string):
+    stack = []
+
+    for s in string:
+        if s in "([":
+            stack.append(s)
+        elif s in ")]":
+            if len(stack) > 0 and stack[-1] == pars[s]:
+                stack.pop()
+            else:
+                return False
+    return len(stack) == 0
