fb6252ccaaf6b7c18481c11d860d6270f0335b3d
DmitShu <dmitshu@gmail.com>
2022-04-15 11:17:02 +0300

    17 test tree

--- Data/tree.py
+++ Data/tree.py
@@ -22,6 +22,36 @@ class BinaryTree:
             self.right_child = new_child
         return self
 
+    #Префиксный
+    def pre_order_pr(self):
+        print(self.value)  # процедура обработки
+
+        if self.left_child is not None:  # если левый потомок существует
+            self.left_child.pre_order_pr()  # рекурсивно вызываем функцию
+
+        if self.right_child is not None:  # если правый потомок существует
+            self.right_child.pre_order_pr()  # рекурсивно вызываем функцию
+
+    #Постфиксный
+    def post_order_po(self):
+        if self.left_child is not None:  # если левый потомок существует
+            self.left_child.post_order_po()  # рекурсивно вызываем функцию
+
+        if self.right_child is not None:  # если правый потомок существует
+            self.right_child.post_order_po()  # рекурсивно вызываем функцию
+
+        print(self.value)  # процедура обработки
+
+    #Инфиксный
+    def in_order_if(self):
+        if self.left_child is not None:  # если левый потомок существует
+            self.left_child.in_order_if()  # рекурсивно вызываем функцию
+
+        print(self.value)  # процедура обработки
+
+        if self.right_child is not None:  # если правый потомок существует
+            self.right_child.in_order_if()  # рекурсивно вызываем функцию
+
 # создаём корень и его потомков /7|2|5\
 node_root = BinaryTree(2).insert_left(7).insert_right(5)
 # левое поддерево корня /2|7|6\
@@ -33,5 +63,9 @@ node_5 = node_root.right_child.insert_right(9)
 # левое поддерево предыдущего узла корня /4|9|\
 node_9 = node_5.right_child.insert_left(4)
 
-print(node_root.value)
-print(node_root.left_child.value)
+
+node_root.pre_order_pr()
+print()
+node_root.post_order_po()
+print()
+node_root.in_order_if()
