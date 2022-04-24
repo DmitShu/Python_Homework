74fd028b85bb3e6867f20051949c7816d7dc8f68
DmitShu <dmitshu@gmail.com>
2022-04-24 21:13:15 +0300

    currency bot stage final

--- Currency_bot/bot_app.py
+++ Currency_bot/bot_app.py
@@ -11,7 +11,7 @@ bot = telebot.TeleBot(Bot_Extensions.get_token())
 @bot.message_handler(commands=['start', 'help'])
 def handle_help(message: telebot.types.Message):
     bot.send_message(message.chat.id, f"Добро пожаловать, {message.chat.username}!\n\n"
-                                      f"Чтобы начать работу, введите команду в следующем формате:\n"
+                                      f"Чтобы узнать стоимость, введите команду в следующем формате:\n"
                                       f"<имя валюты цену которой нужно узнать> <имя валюты в которой надо узнать цену> <количество валюты> (USD RUB 1000)\n"
                                       f"Допускается не указывать количество (USD RUB)\n\n"
                                       f"список доступных валют можно узнать по команде /values")
