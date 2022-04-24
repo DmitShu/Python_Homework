4379c05ee00edae2924130198636a305b417f565
DmitShu <dmitshu@gmail.com>
2022-04-24 17:23:43 +0300

    currency bot stage 5

--- Currency_bot/bot_app.py
+++ Currency_bot/bot_app.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(Bot_Extensions.get_token())
 def handle_help(message: telebot.types.Message):
     bot.send_message(message.chat.id, f"Добро пожаловать, {message.chat.username}!\n\n"
                                       f"Чтобы начать работу, введите команду в следующем формате:\n"
-                                      f"<имя валюты цену которой нужно узнать> <имя валюты в которой надо узнать цену> <количество валюты>\n\n"
+                                      f"<имя валюты цену которой нужно узнать> <имя валюты в которой надо узнать цену> <количество валюты> (USD RUB 1000)\n\n"
                                       f"список доступных валют можно узнать по команде /values")
 
 # Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
@@ -23,17 +23,28 @@ def handle_values(message: telebot.types.Message):
 # Обрабатываются все сообщения
 @bot.message_handler(content_types=['text'])
 def do_convert(message: telebot.types.Message):
-    values = message.text.split()
+    try:
+        values = message.text.split()
 
-    if len(values) != 3:
-        raise APIException('Параметров должно быть три.')
+        if len(values) != 3:
+            raise APIException('"Параметров должно быть три"')
 
-    quote, base, amount = values
-    total = Bot_Extensions.convert(quote, base, amount)
+        quote, base, amount = values
+        total = Bot_Extensions.get_price(quote, base, amount)
+    except APIException as ex:
+        bot.send_message(message.chat.id, f'Ошибка:\n{ex}\n'
+                                          f'Введите команду в следующем формате:\n'
+                                          f'<имя валюты цену которой нужно узнать> <имя валюты в которой надо узнать цену> <количество валюты> (USD RUB 1000)\n\n'
+                                          f'список доступных валют можно узнать по команде /values')
 
-    repl = f'Цена {amount} {quote} в {base} = {round(total, 2)}'
-    bot.send_message(message.chat.id, repl)
+    except Exception as ex:
+        bot.send_message(message.chat.id, f'Ошибка:\n{ex}\n'
+                                          f'Попробуйте команду /help\n'
+                                          f'Если ничего не помогло, обратитесь к разработчику.')
 
+    else:
+        repl = f'Цена {amount} {quote} в {base} = {round(total, 2)}'
+        bot.send_message(message.chat.id, repl)
 
 bot.polling(none_stop=True)
 
--- Currency_bot/bot_extensions.py
+++ Currency_bot/bot_extensions.py
@@ -8,33 +8,39 @@ class APIException(Exception):
 class Bot_Extensions:
     @staticmethod
     def get_token():
-        tokf = 'data\Token'
 
-        with open(tokf) as f:
-            return (f.read())
+        with open('data\Token') as t:
+            return (t.read())
 
     @staticmethod
-    def convert(quote: str, base: str, amount: str):
+    def get_price(quote: str, base: str, amount: str):
 
         if quote == base:
-            raise APIException(f'Введены одинаковые валюты {base}.')
+            raise APIException(f'"Введены одинаковые валюты {base} {base}"')
 
         try:
             quote_ticker = cur[quote]
         except KeyError:
-            raise APIException(f'Не удалось обработать валюту {quote}.')
+            raise APIException(f'"Не удалось обработать валюту {quote}"')
 
         try:
             base_ticker = cur[base]
         except KeyError:
-            raise APIException(f'Не удалось обработать валюту {base}.')
+            raise APIException(f'"Не удалось обработать валюту {base}"')
 
         try:
             amount = float(amount)
-        except KeyError:
-            raise APIException(f'Не удалось обработать количество {amount}.')
+        except:
+            raise APIException(f'"Не удалось обработать количество {amount}"')
 
-        req = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total = json.loads(req.content)[cur[base]] * amount
+        try:
+            req = requests.get(f'https://min-api.crypto compare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        except:
+            raise APIException(f'"Сервер не отвечает"')
+
+        try:
+            total = json.loads(req.content)[cur[base]] * amount
+        except:
+            raise APIException(f'"Неожиданный ответа сервера: {json.loads(req.content)}"')
 
         return total
